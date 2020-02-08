b9147e5e7133069ca8677d7330235bf05d769d9b
Leonid <lifeisgym@gmail.com>
2020-02-09 00:00:54 +0300

    edit adaptive design of main page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,11 +5,11 @@ import {NavLink} from "react-router-dom";
 const Header = () => {
     return (
         <div className={css.header + ' header'}>
-			<nav className={css.nav}>
-                <NavLink className={css.item} activeClassName={css.active} to="/profile">Профиль</NavLink>
-                <NavLink className={css.item} activeClassName={css.active} to="/skills">Навыки</NavLink>
-                <NavLink className={css.item} activeClassName={css.active} to="/experience">Опыт</NavLink>
-                <NavLink className={css.item} activeClassName={css.active} to="/portfolio">Портфолио</NavLink>
+			<nav className={css.nav + ' header_nav'}>
+                <NavLink className={css.item + ' header_item'} activeClassName={css.active} to="/profile">Профиль</NavLink>
+                <NavLink className={css.item + ' header_item'} activeClassName={css.active} to="/skills">Навыки</NavLink>
+                <NavLink className={css.item + ' header_item'} activeClassName={css.active} to="/experience">Опыт</NavLink>
+                <NavLink className={css.item + ' header_item'} activeClassName={css.active} to="/portfolio">Портфолио</NavLink>
             </nav>
 		</div>
     )
