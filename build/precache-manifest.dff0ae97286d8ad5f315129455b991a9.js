3ac9d3f5e9a27bc1ebbe58832957534230f41286
Leonid <lifeisgym@gmail.com>
2020-08-25 23:33:09 +0300

    finish commit with build dir

---
+++ build/precache-manifest.dff0ae97286d8ad5f315129455b991a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f30849a92493975d3eabfbbe4a4110",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab5911bfab531799bab6",
+    "url": "/static/css/main.e6a52a70.chunk.css"
+  },
+  {
+    "revision": "3032e08a3c2b4e0d5908",
+    "url": "/static/js/2.be4429dc.chunk.js"
+  },
+  {
+    "revision": "ab5911bfab531799bab6",
+    "url": "/static/js/main.9e6f2c0c.chunk.js"
+  },
+  {
+    "revision": "b712a1313888196de763",
+    "url": "/static/js/runtime-main.fd45feea.js"
+  }
+]);
